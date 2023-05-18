a83a8d4db28a5bfd6ddf608ff1b184d6cd09b056
ekkoo <97926809+kong030813@users.noreply.github.com>
2023-05-18 19:12:13 +0800

    edusrc.py

--- edusrc.py
+++ edusrc.py
@@ -9,31 +9,30 @@ def rankchange():
                "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
                "Accept-Encoding": "gzip, deflate", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
     url = "https://src.sjtu.edu.cn/profile/15013/"
+    response1 = requests.get(url, headers=headers).text
+    rank1 = re.findall(r'Rank： .*', response1)
+    name1 = re.findall(r'<h3 class="am-panel-title am-fl">.*', response1)
+    name2=str(name1) #split函数只支持字符串类型截取
+    name=name2.split('>',1)[1].split('的')[0]#截取用户名
     while(1):
-        response1 = requests.get(url, headers=headers).text
-        rank1 = re.findall(r'Rank： .*', response1)
-        name1 = re.findall(r'<h3 class="am-panel-title am-fl">.*', response1)
-        name2=str(name1) #split函数只支持字符串类型截取
-        name=name2.split('>',1)[1].split('的')[0]#截取用户名
-        while(1):
-            response = requests.get(url,headers=headers).text
-            rank = re.findall(r'Rank： .*', response)
-            print(rank)
-            if(rank!=rank1):
-                text_title = name+'您的EDUSRC漏洞审核通过啦(*^▽^*)'
-                text_content = "在另一条时间线，这么做是对的  \n 加油！少年\n"+"详情请点击"+url
-                sendKey = 'xxxxxxxxxxxxxxxxxxx'
+        response = requests.get(url,headers=headers).text
+        rank = re.findall(r'Rank： .*', response)
+        print(rank)
+        if(rank!=rank1):
+            text_title = name+'您的EDUSRC漏洞审核通过啦(*^▽^*)'
+            text_content = "在另一条时间线，这么做是对的  \n 加油！少年\n"+"详情请点击"+url
+            sendKey = 'SCT210463TFGHBI8F8NZgpOQDxZSSpXW01'
 
-                url = f"https://sctapi.ftqq.com/{sendKey}.send"
-                headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-                data = {
-                    'text': f"{text_title}",
-                    'content': f"{text_content}"
-                }
-                response = requests.post(url, data=data)
-                if json.loads(response.text)["data"]['error'] == 'SUCCESS':
-                    print("消息推送成功")
-            else:
-                print("服务正在运行中...")
+            url = f"https://sctapi.ftqq.com/{sendKey}.send"
+            headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+            data = {
+                'text': f"{text_title}",
+                'content': f"{text_content}"
+            }
+            response = requests.post(url, data=data)
+            if json.loads(response.text)["data"]['error'] == 'SUCCESS':
+                print("消息推送成功")
+        else:
+            print("服务正在运行中...")
 
-    rankchange()
+rankchange()
